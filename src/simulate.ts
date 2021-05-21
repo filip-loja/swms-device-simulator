852a09559f85187f68b06536ec22070399e9d07c
Filip Loja <filiploja@gmail.com>
2021-05-21 02:08:30 +0200

    Add Azure dependencies and start sending messages

--- src/simulate.ts
+++ src/simulate.ts
@@ -1,14 +1,27 @@
 import { ExcelSheet } from './types'
+import { Mqtt } from 'azure-iot-device-mqtt'
+import { Client, Message } from 'azure-iot-device'
 
 export const simulate = (sheet: ExcelSheet, interval: number) => {
+	const connectionStr = sheet.data.shift()[0].trim()
+	const client = Client.fromConnectionString(connectionStr, Mqtt);
+
 	console.log(`Simulation for "${sheet.name}" started.`)
+
 	let rowIndex = 0
 	let finalIndex = sheet.data.length
 
 	let intervalRef = setInterval(() => {
-		const message = sheet.name + ';' + sheet.data[rowIndex].join(';')
-		// TODO send mesage to azure
-		console.log('  -> ', message)
+		const messageStr = sheet.name + ';' + sheet.data[rowIndex].join(';')
+		const message = new Message(messageStr)
+		console.log('  -> ', messageStr)
+
+		client.sendEvent(message, err => {
+			if (err) {
+				console.error('  -> Error: ' + err.toString())
+			}
+		})
+
 		rowIndex++
 		if (rowIndex === finalIndex || !sheet.data[rowIndex].length) {
 			clearInterval(intervalRef)
